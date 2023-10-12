2ec5feb5e16927eed42e0ce011788c2bf9bb2480
GUNTUKU NARESH <guntuku001@gmail.com>
2023-10-12 21:57:38 +0530

    Basic JS

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+// var str="naresh";
+// var str1="suresh";
+// console.log(str.length);
+// console.log(str +" and " + str1 +" are brothers");
+// let x="narehs7ajasndjasdam "
+// console.log("You have entered text of length "+x.length+" and you have left with remaining "+(10-x.length));
+// var name="naresh"
+// console.log(name.slice(0,3));
+// var name="naresh";
+// var ans=name.toUpperCase();
+// console.log(ans);
+// console.log(ans.toLowerCase());
+// console.log(3+5*2);
+console.log(3+'3');
+console.log('3'+'3');
+console.log("3"+"3");
