d2577e1fd84ff165cda288d9e60fc66902088293
GUNTUKU NARESH <guntuku001@gmail.com>
2023-10-15 22:57:36 +0530

    fun.js updated

---
+++ JS/fun.js
@@ -0,0 +1,12 @@
+function add(a1,a2){
+    return a1+a2;
+}
+console.log(add(1,2))
+function mul(a1,a2){
+    return a1*a2;
+}
+console.log(mul(2,2))
+function div(a1,a2){
+    return a1/a2;
+}
+console.log(div(4,2))
